62ea87b05b4dcba8a08ae8d714902b5eec20f687
Evgenii_Lobachev <lobachev_e_96@mail.ru>
2025-09-02 00:15:54 +0300

    install pytest

--- src/calculate.py
+++ src/calculate.py
@@ -1,13 +1,25 @@
-def calculate_taxes(prices: list[float], tax_rate: float) -> list[float]:
+def calculate_taxes(prices: list[float], tax_rate: float, *, discount: float = 0) -> list[float]:
     """Функция вычисляет стоимость товаров с учётом налога."""
     new_list = []
-    for price in prices:
-        if price <= 0:
-            raise ValueError ('Неверная цена')
-    if tax_rate <= 0 or tax_rate >= 100:
-            raise ValueError('Неверный налоговый процент')
+    for arg in [ tax_rate, discount]:
+        if not isinstance(arg, int| float):
+            raise TypeError('Неверный тип данных')
+    if not isinstance(prices, list):
+        raise TypeError('Неверный список цен')
     else:
         for price in prices:
+            if not isinstance(price, int or float):
+                raise TypeError('Неверная цена')
+            elif price <= 0:
+                raise ValueError('Неверная цена')
+        if tax_rate < 0 or tax_rate >= 100:
+                raise ValueError('Неверный налоговый процент')
+        if discount < 0:
+            raise ValueError('Скидка не может быть с минусом')
+        for price in prices:
             new_price = price + (price * tax_rate) / 100
-            new_list.append(new_price)
+            resul_price = (new_price - (new_price * discount) / 100)
+            new_list.append(round(float(resul_price), 2))
         return new_list
+
+
--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -3,30 +3,61 @@ import pytest
 from src.calculate import calculate_taxes
 
 
-@pytest.mark.parametrize('tax_rate, resul', [(20, [120.0, 240.0]),
-                                             (30, [130.0, 260.0]),
-                                             (10, [110.0, 220.0])
+@pytest.mark.parametrize('tax_rate, resul', [(20, [120.00, 240.00]),
+                                             (30, [130.00, 260.00]),
+                                             (10, [110.00, 220.00])
                                              ])
-
 def test_calculate_taxes(prices1, tax_rate, resul):
     assert calculate_taxes(prices1, tax_rate) == resul
+
 
 def test_calculate_taxes_prices1_zero():
     with pytest.raises(ValueError) as info:
         calculate_taxes([0], 10)
 
+
 def test_calculate_taxes_tax_rate_100(prices1):
     with pytest.raises(ValueError) as info:
         calculate_taxes(prices1, 100)
 
+
 def test_calculate_taxes_tax_rate_zero(prices1):
     with pytest.raises(ValueError) as info:
-        calculate_taxes(prices1, 0)
+        calculate_taxes(prices1, -1)
+
 
 def test_calculate_taxes_tax_rate_200(prices1):
     with pytest.raises(ValueError) as info:
         calculate_taxes(prices1, 200)
 
+
 def test_calculate_taxes_tax_rate_minus_50(prices1):
     with pytest.raises(ValueError) as info:
         calculate_taxes(prices1, -50)
+
+
+def test_calculate_taxes_discount(prices1):
+    assert calculate_taxes(prices1, 10, discount = 5) == [104.5, 209.0]
+
+def test_calculate_taxes_discount_4_5(prices1):
+    assert calculate_taxes(prices1, 10, discount = 4.5) == [105.05, 210.1]
+
+
+def test_calculate_taxes_prices1e_not_int():
+    with pytest.raises(TypeError) as info:
+        calculate_taxes("prices1", -50)
+
+def test_calculate_taxes_tax_rate_not_int(prices1):
+    with pytest.raises(TypeError) as info:
+        calculate_taxes(prices1, "False")
+
+def test_calculate_taxes_tax_rate_not_f(prices1):
+        assert calculate_taxes(prices1, False) == [100, 200]
+
+def test_calculate_taxes_tax_rate_not_dis(prices1):
+    with pytest.raises(ValueError) as info:
+        calculate_taxes(prices1, 0, discount = -20)
+
+def test_calculate_taxes_prices1e_not_i():
+    with pytest.raises(TypeError) as info:
+        calculate_taxes(["prices1", False], 0)
